60243cc52e8ba2be20a2c31b88995ad55b60f5fa
buhandrey <buh.andrey@yandex.ru>
2026-04-22 15:45:23 +0400

    abstract class noise created

---
+++ noise.h
@@ -0,0 +1,30 @@
+#ifndef NOISE_H
+#define NOISE_H
+
+#include "process.h"
+
+class noise : public process {
+protected:
+    long idum; /*Зерно шума*/
+    virtual double generator () = 0;
+public:
+    noise() {
+        modelname = "noise";
+    };
+    ~noise() = default;
+    void step () {
+        if (initialized) {
+            marker++;
+            if (marker == val.size())
+                marker = 0;
+            discrete_time++;
+            val[marker] = generator ();
+            substep();
+        }
+        else {
+            throw std::invalid_argument("Object noise isn't defined before use\n");
+        }
+    }
+};
+
+#endif
