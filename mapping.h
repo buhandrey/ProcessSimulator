5fc50b73af0438b953165dce0d185e9754d06056
buhandrey <buh.andrey@yandex.ru>
2026-04-22 16:10:39 +0400

    abstract class mapping created

---
+++ mapping.h
@@ -0,0 +1,33 @@
+#ifndef MAPPING_H
+#define MAPPING_H
+
+#include "ds.h"
+
+class mapping : public ds {
+public:
+    mapping() {
+        dt = 1.0;
+        modelname = "mapping";
+    }
+    ~mapping() = default;
+    void step () {
+        if (initialized) {
+            function (0, 0.0, 0);
+            for (long n=0;n<vars.s1();n++) {
+                vars.step(n, all_vars_temp.get(0, 0, n));
+                for (long i=0;i<vars_tilda.s1();i++) {
+                    vars_tilda.step(i, n, all_vars_temp.get(0, i+1, n));
+                }
+            }
+            marker = (marker + 1) % val.size();
+            discrete_time++;
+            calc_amplitude ();
+            substep();
+        }
+        else {
+            throw std::invalid_argument("Object mapping isn't defined before use\n");
+        }
+    }
+};
+
+#endif
